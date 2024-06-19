6e79cb5ae6fbe3963cf7b573ffbcf2e92932220e
Aash <31497659+AashPau@users.noreply.github.com>
2024-06-19 15:05:39 +1000

    created initial version of product schema

---
+++ src/model/product/productSchema.js
@@ -0,0 +1,19 @@
+import { required } from "joi";
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+});
+
+export default mongoose.model("Product", productSchema);
